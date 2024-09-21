aae207a65f4ddef4b2a6a409b4cba6dbdb767fd3
leshayq <oleshadesigner123@gmail.com>
2024-09-21 19:51:55 +0300

    mysql database connected

--- aeon/shop/views.py
+++ aeon/shop/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404, render
 from .models import Category, ProductProxy, Product
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from common.services import all_objects, filter_objects
 
 class IndexView(ListView):
     model = ProductProxy
@@ -12,13 +13,17 @@ class IndexView(ListView):
     context_object_name = 'index_products'
 
     def get_queryset(self) -> QuerySet[Any]:
-        return super().get_queryset().filter(available=True)
-    
+        return filter_objects(ProductProxy.objects, available=True)
+
 class ProductDetailView(DetailView):
     model = ProductProxy
     template_name = 'shop/product_detail.html'
     context_object_name = 'product'
 
     def get_queryset(self):
-        return super().get_queryset().filter(slug=self.kwargs['slug'])
+        return filter_objects(ProductProxy.objects, slug=self.kwargs['slug'])
     
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.object.title
+        return context
