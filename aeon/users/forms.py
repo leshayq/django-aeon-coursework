22d911c6ca21539af270005ece7283fd33b2a9fb
leshayq <oleshadesigner123@gmail.com>
2024-10-01 21:47:39 +0300

    registration

--- aeon/users/forms.py
+++ aeon/users/forms.py
@@ -2,8 +2,16 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.forms import PasswordInput, TextInput, EmailField
+from django.core.exceptions import ValidationError
+from django.contrib import messages
 
 User = get_user_model()
+
+class UserRegisterForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
 
 class UserLoginForm(AuthenticationForm):
     username = forms.EmailField(widget=TextInput(attrs={'placeholder': 'Ваш e-mail', 'id': 'id_email'}))
--- aeon/users/views.py
+++ aeon/users/views.py
@@ -1,8 +1,23 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegisterForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(form.cleaned_data['email'])
+            # Save the User object
+            new_user.save()
+            return render(request, 'shop/index.html')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
 def login_user(request):
     form = UserLoginForm()
