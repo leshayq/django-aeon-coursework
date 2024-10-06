78c49b00ba76a9927f56a35a9a4295030c0b9410
leshayq <oleshadesigner123@gmail.com>
2024-10-06 18:50:40 +0300

    user registration, abstrct_bs_user

--- aeon/users/admin.py
+++ aeon/users/admin.py
@@ -4,5 +4,8 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-
+    list_display = ('email', 'first_name', 'is_staff', 'is_active', 'is_superuser')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'groups')
+    search_fields = ('email',)
+    ordering = ('email',)
 admin.site.register(CustomUser, CustomUserAdmin)
--- aeon/users/views.py
+++ aeon/users/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
+from .models import CustomUser
 from .forms import UserLoginForm, UserRegisterForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib import messages
+
 
 def login_user(request):
     form = UserLoginForm()
@@ -27,3 +29,26 @@ def login_user(request):
         'form': form,
     }
     return render(request, 'users/login.html', context)
+
+def register_user(request):
+    if request.user.is_authenticated:
+        return redirect('shop:main')
+    
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            user_email = form.cleaned_data.get('email')
+            user_first_name = form.cleaned_data.get('first_name')
+            user_password = form.cleaned_data.get('password1')
+
+            user = CustomUser.objects.create_user(email=user_email, password=user_password, first_name=user_first_name)
+
+            user.is_active = False
+            user.save()
+            
+            return redirect('shop:main')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, "users/register.html", {"form":form})
